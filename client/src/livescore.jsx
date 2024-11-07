6c415248eb31e1c82331e3e32a2240c866c6ac86
Jai Kishan <72691369+kumarjaikishan@users.noreply.github.com>
2024-11-07 13:06:09 +0530

    gjhgj

--- client/src/livescore.jsx
+++ client/src/livescore.jsx
@@ -15,7 +15,7 @@ const TournamentScore = () => {
     // const ws = new WebSocket('ws://localhost:5006');
     // const ws = new WebSocket('ws://localhost:5006');
     // const ws = new WebSocket('wss://127.0.0.1:5006');
-    const ws = new WebSocket('http://127.0.0.1:5006');
+    const ws = new WebSocket('https://127.0.0.1:5006/ws');
     // const ws = new WebSocket('/ws');
     // const ws = new WebSocket('/');
 
